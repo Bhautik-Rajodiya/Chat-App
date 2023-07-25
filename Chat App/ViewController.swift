8c48dc9de8b0934049be51113bd24679271cf71e
Bhautik Rajodiya <bhautikrajodiya12345@gmail.com>
2023-07-25 18:37:06 +0530

    Initial Commit

---
+++ Chat App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chat App
+//
+//  Created by Bhautik Rajodiya on 25/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
